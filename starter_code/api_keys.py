1a160bd42b5ac215bf1cf7cf538d645794c58eb3
e'ddddddddddd <98539748+Ejwpls@users.noreply.github.com>
2022-04-26 23:46:28 +0800

    three-quater done

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e7b86028c88b231cd40d749ba3dd14f0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+gkey = "AIzaSyCYy6tQphXA_bhu6k2Isa-0Rbaz3WhX74E"
